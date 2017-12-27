6654dd6840bb40a6700b3ac603c1c67e87f7fa5f
brandonndsi <brandon-ndsi@hotmail.com>
2017-12-27 14:39:05 -0600

    Menusillo css

--- view/catalogoview/CatalogoView.php
+++ view/catalogoview/CatalogoView.php
@@ -5,7 +5,7 @@
         <meta charset="UTF-8">
         <title> VentaRepuestos Online </title>
 
-        <link rel="stylesheet" href="">  
+        <link rel="stylesheet" href="../../css/styles.css">  
 
         <script src=""></script>        
 
@@ -13,21 +13,25 @@
     <body>
 
         <header>
+            <nav class="navbar navbar-default">
+                <div>
+                    <div>
+                        <ul class="nav navbar-nav">
+                            <li><a href="../../index.php"> Inicio</a></li>
+                            <li><a href="#infoventaonline" > Acerca de Ventas Online</a></li>
 
-            <div>
-                <ul>
-                    <li><a href="../../index.php"> Inicio</a></li>
-                    <li><a href=""> Acerca de Ventas Online</a></li>
-                </ul>
-                <ul>
-                    <li>
-                        <a href=""><strong>Registrar</strong></a>
-                    </li>
-                    <li>
-                        <a href="../../view/loginview/LoginView.php"><strong>Ingresar</strong></a>
-                    </li>
-                </ul>
-            </div>
+                        </ul>
+                        <ul class="nav navbar-nav navbar-right">
+                            <li>
+                                <a href="" ><strong>Registrar</strong></a>
+                            </li>
+                            <li>
+                                <a href="../../view/loginview/LoginView.php" ><strong>Ingresar</strong></a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
         </header>
 
         <main>
