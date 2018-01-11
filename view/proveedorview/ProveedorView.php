043266faea82014c1ce9d3f13c2ec74d87ad498e
12345star <yoansalas76@gmail.com>
2018-01-10 23:55:22 -0600

    modificando el proveedor menu version beta

--- view/proveedorview/ProveedorView.php
+++ view/proveedorview/ProveedorView.php
@@ -5,47 +5,20 @@
         <meta charset="UTF-8">
         <title>Proveedor</title>
         <!-- CSS-->
-        <link rel="stylesheet" type="text/css" href="../../css/styles.css">
+        <link rel="stylesheet" type="text/css" href="../../css/proveedor.css">
         <!-- JS-->
         <script src="../../js/jquery-1.10.2.js"></script>
 
     </head>
     <body>
-    	<header>
-    		<input type="checkbox" id="btn-menu">
-    		<label for="btn-menu">MENU</label>
-    	<nav>
-    		<ul class="menu">
-    			<li><a href="#">Regresar</a></li>
-    			<li><a href="#">Nuevo</a></li>
-    			<li><a href="#">Ver</a></li>
-    			<li><a href="#">Actualizar</a></li>
-    		</ul>
-    	</nav>
-    </header>
-    	<div id="nuevoProveedor">
-    		
-            <p>Cedula:<input type="text" name="cedula" placeholder="x-xxxx-xxx"></p>
-            <p>Nombre:<input type="text" name="nombre" placeholder="pedro"></p>
-            <p>Apellido1:<input type="text" name="apellido1" placeholder="castro"></p>
-            <p>Apellido2:<input type="text" name="apellido2" placeholder="salazar"></p>
-            <p>Telefono:<input type="text" name="telefono" placeholder="xxxx-xxxx"></p>
-            <p>Correo:<input type="text" name="correo" placeholder="pedro@gmail.com"></p>
-            <p>Producto:<input type="text" name="producto" placeholder="id producto"></p>
-            <p><input type="submit" name="nuevo" onclick="nuevoProveedor()"></p>
-            
-    	</div>
-    	<div id="actualizarProveedor">
-    		<p>Cedula:<input type="text" name="cedula" placeholder="x-xxxx-xxx"></p>
-            <p>Nombre:<input type="text" name="nombre" placeholder="pedro"></p>
-            <p>Apellido1:<input type="text" name="apellido1" placeholder="castro"></p>
-            <p>Apellido2:<input type="text" name="apellido2" placeholder="salazar"></p>
-            <p>Telefono:<input type="text" name="telefono" placeholder="xxxx-xxxx"></p>
-            <p>Correo:<input type="text" name="correo" placeholder="pedro@gmail.com"></p>
-            <p>Producto:<input type="text" name="producto" placeholder="id producto"></p>
-            <p><input type="submit" name="nuevo" onclick="actualizarProveedor()"></p>
-    	</div>
-    	<div id="detallesProveedor"></div>
+<ul>
+  <li><a class="active" href="../../index.php">Menu</a></li>
+  <li style="float:right"><a class="active" href="#about">Salir</a>
+  <!--<li><a href="#news">News</a></li>
+  <li><a href="#contact">Contact</a></li>
+  <li><a href="#about">About</a></li>-->
+</ul>
+    
 
     </body>
 </html>
