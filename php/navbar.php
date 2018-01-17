d6e54396867d9f35c6441d779b4d87867e651eca
GitHubJR08 <32047343+GitHubJR08@users.noreply.github.com>
2018-01-17 17:01:50 -0600

    Add files via upload

---
+++ php/navbar.php
@@ -0,0 +1,33 @@
+<nav class="navbar navbar-default" role="navigation">
+    <div class="container">
+    <!-- Brand and toggle get grouped for better mobile display -->
+    <div class="navbar-header">
+    <!--<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
+      <span class="sr-only">Toggle navigation</span>
+      <span class="icon-bar"></span>
+      <span class="icon-bar"></span>
+      <span class="icon-bar"></span>
+    </button>-->
+    <!--<a class="navbar-brand" href="./">
+        <b>VOLVER AL INICIO</b>
+    </a>-->
+    </div>
+
+    <!-- Collect the nav links, forms, and other content for toggling -->
+    <div class="collapse navbar-collapse navbar-ex1-collapse">
+        <!--<center><ul class="nav navbar-nav">
+            <li class="enlace">
+                <a class="ver" href="./ver.php">Presione aquí para ver la lista de clientes</a>
+            </li>
+            </ul></center>--><br>
+    <form class="navbar-form navbar-left" role="search" action="./buscar.php">
+            <center><div class="form-group">
+            <input type="text" name="s" class="form-group" placeholder="Buscar Clientes" value="">
+            </div><br>
+            <button type="submit" class="btn btn-default">Buscar
+                <i class="glyphicon glyphicon-search"></i>&nbsp;
+            </button></center><br>
+    </form>
+    </div><!-- /.navbar-collapse -->
+    </div>
+</nav>
