5cd327c808f8afef341b9d6b62c7f4b66e3d5b2b
brandonndsi <brandon-ndsi@hotmail.com>
2018-01-17 20:17:41 -0600

    Actualizacion de usuario/cliente.3

--- domain/clientes/Clientes.php
+++ domain/clientes/Clientes.php
@@ -4,6 +4,28 @@ include '../../domain/personas/Personas.php';
 
 class Clientes extends Personas {
     
+    private $clienteid;
+    private $clientedireccionexacta;
+    
+    public function Clientes($clienteid, $clientedireccionexacta) {
+        $this->clienteid = $clienteid;
+        $this->clientedireccionexacta = $clientedireccionexacta;
+    }
+    public function getClienteid() {
+        return $this->clienteid;
+    }
+
+    public function getClientedireccionexacta() {
+        return $this->clientedireccionexacta;
+    }
+
+    public function setClienteid($clienteid) {
+        $this->clienteid = $clienteid;
+    }
+
+    public function setClientedireccionexacta($clientedireccionexacta) {
+        $this->clientedireccionexacta = $clientedireccionexacta;
+    }
 }
 
 ?>
