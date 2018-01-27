b3b45951ae15f47538d01996b0b83dbf585c566e
12345star <yoansalas76@gmail.com>
2018-01-27 00:42:10 -0600

    datos de administrador el help terminado

---
+++ js/administrador.js
@@ -0,0 +1,8 @@
+
+function cerrarHelp(){
+	document.getElementById("help").style.display="none";
+}
+function abrirModal(){
+	document.getElementById("help").style.transform="translateY(0%)";
+	//document.getElementById("contenedor_help").style.transform="translateY(0%)";
+}
--- view/ventanaprincipalview/VentanaPrincipalView.php
+++ view/ventanaprincipalview/VentanaPrincipalView.php
@@ -12,6 +12,7 @@
         <!-- js -->
         <script type="text/javascript" src="../../js/jquery-1.10.2.js"></script>
         <script type="text/javascript" src="../../js/imagen.js"></script>
+        <script type="text/javascript" src="../../js/administrador.js"></script>
 
     </head>
     <script>
@@ -34,9 +35,9 @@
             <div id="navegador">
             <input type="checkbox" id="menu-bar">
             <label class="icon-menu" for="menu-bar"></label>
-            <a><label class="icon-help-with-circle"></label></a>
-            <a><label class ="icon-search"></label></a>
-            <a><label class="icon-bell"></label></a>
+            <a href="javascript:abrirModal();"><label class="icon-help-with-circle"></label></a>
+            <a href="#"><label class ="icon-search"></label></a>
+            <a href="#"><label class="icon-bell"></label></a>
 
             <div id="menu">
                 <h4><?php echo " " .$persona[1]; ?></h4>
@@ -71,5 +72,23 @@
             </div> 
         </div>
         <input type="hidden" id="personaid" value="<?php echo $id; ?>">
+        <!-- comensando con el modal de help -->
+        <div id="help">
+            <div id="contenedor_help">
+            <div id="titulo_help">
+               <h2>Ayuda</h2> 
+            </div>
+            <div id="body_help">
+               <p>El menu de apciones te proporciona varios modulos los caules puedes consultar y actualizar datos de los mismos en la base de datos,entre los modulos estan:<br>
+               <b>Registrar compra:</b> la cual consiste en poder generar la compra a un cliente en genera.<br>
+               <b>Orden de compra:</b> Consiste en poder ver los pedidos de los usuarios.<br>
+               <b>Empleado:</b> Presenta todos datos de editar,eliminar,actualizar y crear empleado, asi como la facturacion,tipo de empleado,cliente,proveedory el catalogo.
+                .</p> 
+            </div>
+            <div id="footer_help">
+                <a href=""><span class=" icon-checkmark"></span></a>
+            </div>
+        </div>
+        </div>
     </body>
 </html>
