65fcc39bd1a390ab3946cf171c79779a0ccaef8a
12345star <yoansalas76@gmail.com>
2018-01-28 22:45:04 -0600

    datos del adminstrador de las cosas del help

--- js/administrador.js
+++ js/administrador.js
@@ -1,8 +1,16 @@
 
 function cerrarHelp(){
-	document.getElementById("help").style.display="none";
+	document.getElementById("help").style.transform="translateY(-150%)";
 }
 function abrirModal(){
 	document.getElementById("help").style.transform="translateY(0%)";
+	//document.getElementById("contenedor_help").style.transform="translateY(0%)";
+}
+
+function cerrarBuscar(){
+	document.getElementById("buscar").style.transform="translateY(-150%)";
+}
+function abrirBuscar(){
+	document.getElementById("buscar").style.transform="translateY(0%)";
 	//document.getElementById("contenedor_help").style.transform="translateY(0%)";
 }
--- view/ventanaprincipalview/VentanaPrincipalView.php
+++ view/ventanaprincipalview/VentanaPrincipalView.php
@@ -36,7 +36,7 @@
             <input type="checkbox" id="menu-bar">
             <label class="icon-menu" for="menu-bar"></label>
             <a href="javascript:abrirModal();"><label class="icon-help-with-circle"></label></a>
-            <a href="#"><label class ="icon-search"></label></a>
+            <a href="javascript:abrirBuscar();"><label class ="icon-search"></label></a>
             <a href="#"><label class="icon-bell"></label></a>
 
             <div id="menu">
@@ -89,6 +89,22 @@
                 <a href=""><span class=" icon-checkmark"></span></a>
             </div>
         </div>
+        </div>
+        <!-- terminacion del modal de ayuda al administrador -->
+        <!-- comenzando con el modal de la busqueda de las funciones del administrador-->
+        <div id="buscar">
+            <div id="contenedor_buscar">
+                <div id="titulo_buscar">
+                <h2>¿Qué estas buscando?</h2>
+                </div>
+                <div id="body_buscar">
+                <input type="text" id="cont_buscar">
+                </div>
+                <div id="footer_buscar">
+                    <a href="" id="busP"><span class="icon-search"></span></a>
+                    <a href="javascript:cerrarBuscar();" id="busE"><span class="icon-cross"></span></a>
+                </div>
+            </div>
         </div>
     </body>
 </html>
