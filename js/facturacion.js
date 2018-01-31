e3587f314b4c2aa9d885686750da38f450ead412
12345star <yoansalas76@gmail.com>
2018-01-30 21:55:28 -0600

    datos de factura modal nuevo

--- js/facturacion.js
+++ js/facturacion.js
@@ -5,10 +5,54 @@ window.onload=function(){
             theme: "dark-thin"//,
             //scrollButtons:{ enable: true}
             });
-    document.getElementById("tabla_principal").mCustomScrollbar({
-        theme: "dark-thin"
+    $("#tabla_principal").mCustomScrollbar({
+        theme: "dark-thin"//,
+        //scrollButtons:{ enable: true}
+    });
+    $("#nuevofactura").mCustomScrollbar({
+        theme: "dark-thin"//,
+        //scrollButtons:{ enable: true}
     });
 };
+/*comensando con las validaciones de los modales.*/
+/*modal de nuevo factura.*/
+function abrirNuevoFactura(){
+    cerrarTodo();
+    //.style.transform="translateY(-150%)";
+document.getElementById("nuevofactura").style.display="block";
+}
+function cerrarNuevoFactura(){
+document.getElementById("nuevofactura").style.display="none";
+}
+/*terminacion de modal de nueva factura*/
+/*nuevas btn abrir nuevo factura*/
+function abrirBTNNuevoFactura(){
+document.getElementById("body_principal").style.display="block";
+}
+function cerrarBTNNuevoFactura(){
+document.getElementById("body_principal").style.display="none";
+}
+/*nuevas btn abrir nuevo terminar*/
+/*pricipal de factura  tabla cerrar*/
+function abrirTablaFactura(){
+    cerrarTodo();
+    abrirBTNNuevoFactura();
+document.getElementById("tabla_principal").style.display="block";
+}
+function cerrarTablaFactura(){
+document.getElementById("tabla_principal").style.display="none";
+}
+/*principal de fatura tabla de cerrar terminar*/
+/*funcion de guardar de factura en el btnGuardar*/
+function BTNGuardarNuevoFactura(){
+   abrirTablaFactura(); 
+}
+/*funtion de guardar de factura terminar*/
+function cerrarTodo(){
+cerrarBTNNuevoFactura();
+cerrarNuevoFactura();
+cerrarTablaFactura();
+}
 
 /*funcion de js para mostrar el data table con paginacion*/
 function mostrarFactura() {
--- view/facturaview/FacturaView.php
+++ view/facturaview/FacturaView.php
@@ -34,7 +34,7 @@
 			</div>
 			<div id="body_principal">
 				<label>Factura</label>
-            <input type="submit" class="btn-nuevo" onclick="nuevoMostrar()" value="Nuevo">
+            <input type="submit" class="btn-nuevo" onclick="abrirNuevoFactura()" value="Nuevo">
 			</div>
 			<div class="col-md-12 col-md-offset-2" id="tabla_principal">
 				<table id="lista_factura" class="table table-striped table-bordered" >
@@ -62,6 +62,57 @@
 					</tfoot>
 				</table>
 			</div>
+			<div id="nuevofactura">
+				<div id="nuevofactura_titulo">
+				  <label><span class="icon-clipboard"></span>Nueva Factura</label>
+				</div>
+				  <div id="nuevofactura_body">
+				  		<div id="nombrecliente_body">
+				  			<label>Cliente</label>
+					  		<input type="text" id="nombreCliente" placeholder="Selecciona un cliente" required>
+					  		<input id="idcliente" type='hidden'>
+						</div>
+						<div id="telefono_body">
+				  			<label>Teléfono</label>
+							<input type="text" id="telefono" placeholder="Teléfono" readonly>
+						</div>
+						<div id="email_body">
+							<label>Email</label>
+							<input type="text" class="form-control input-sm" id="mail" placeholder="Email" readonly>
+						</div>
+						<div id="vendedor_body">
+							<label>Vendedor</label>
+								<select id="idvendedor">
+									<option value="1">david</option>
+									<option value="2">angui</option>
+									<option value="3">pablo</option>
+									<option value="4">Carlos</option>
+								</select>
+						</div>
+						<div id="fecha_body">
+							<label>Fecha</label>
+							<input type="text" class="form-control input-sm" id="fecha" value="<?php echo date("d/m/Y");?>" readonly>
+						</div>
+						<div id="pago_body">
+							<label>Pago</label>
+								<select  id="condiciones">
+									<option value="1">Efectivo</option>
+									<option value="2">Cheque</option>
+									<option value="3">Transferencia bancaria</option>
+									<option value="4">Crédito</option>
+								</select>
+						</div>
+				</div>
+				<div id="nuevafactura_footer">
+				<a href="#" id="nuevo_producto"><span class="icon-plus"></span>Nuevo producto</a>
+				<a href="#"><span class="icon-user-plus"></span>Nuevo cliente</a>
+				<a href="#" id="agregar_producto"><span class="icon-search"></span>producto</a>
+				<a href="javascript:BTNGuardarNuevoFactura();"><span class="icon-floppy-disk"></span>Guardar</a>
+				</div>
+		</div>
+		<div id="nuevoProducto">
+			
+		</div>	 
 		
 <?php 
 	include_once '../modalnotificaciones/modalnotificacionadministrador/administradorayuda.php';
